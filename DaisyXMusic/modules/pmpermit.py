5709dd77d258d802f59ce644be7cd78d5c367e46
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-11 09:06:07 +0700

    Update pmpermit.py

--- DaisyXMusic/modules/pmpermit.py
+++ DaisyXMusic/modules/pmpermit.py
@@ -33,7 +33,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
+                "Halo, Saya Adalah Assistent Music @{BOT_USERNAME}\n\n**KIRIM TAUTAN UNDANGAN GRUP ATAU NAMA PENGGUNA JIKA USERBOT TIDAK DAPAT BERGABUNG DENGAN GRUP ANDA**\n\n",
             )
             return
 
