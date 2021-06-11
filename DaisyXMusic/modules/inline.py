b7a503e7b536f7fe7ad1641d1d406aa8d481a2bd
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-11 12:22:26 +0700

    Update inline.py

--- DaisyXMusic/modules/inline.py
+++ DaisyXMusic/modules/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="Ketikan Judul Lagu/Video Yang Dicari",
             switch_pm_parameter="help",
             cache_time=0,
         )
